ef1951d393ee605db3edc5eea042f43417509e94
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-08-08 09:26:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,53 @@
-// complete the JS code
 const nameInput = document.getElementById("name");
 const scoreInput = document.getElementById("score");
-const scores = document.getElementById("scores");
+const scoresDiv = document.getElementById("scores");
 
 // Save score to Local Storage
 function saveScore() {
-  // complete the code here
+  const name = nameInput.value;
+  const score = parseInt(scoreInput.value);
+
+  if (!name || isNaN(score)) {
+    alert("Please enter a valid name and score.");
+    return;
+  }
+
+  const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+  highScores.push({ name, score });
+
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+
+  // Clear inputs
+  nameInput.value = "";
+  scoreInput.value = "";
+
   showScores();
 }
 
 // Show scores in div
 function showScores() {
-  // complete the code
+  const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+  if (highScores.length === 0) {
+    scoresDiv.textContent = "No scores yet";
+    return;
+  }
+
+  scoresDiv.innerHTML = `
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Score</th>
+      </tr>
+      ${highScores
+        .map(
+          score =>
+            `<tr>
+              <td>${score.name}</td>
+              <td>${score.score}</td>
+            </tr>`
+        )
+        .join("")}
+    </table>
+  `;
 }
